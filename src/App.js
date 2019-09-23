7d330f98909fe91ac6093219bbeda7c33ff7ac49
AzoozMac <abdulaziz-003@hotmail.com>
2019-09-23 18:30:39 +0300

    dynamic listing from fake server data

--- src/App.js
+++ src/App.js
@@ -6,12 +6,67 @@ const defaultStyle = {
   color: textColor,
 
 }
+ const fakeServerData = {
+   user: {
+     name: 'Abdulaziz',
+     playlists: [
+       {
+         name: 'My favorites 1',
+         songs: [
+            {name:'song1.1', duration: 1342},
+            {name:'song2.1', duration: 3212},
+            {name: 'song3.1', duration:2123}
+        ]
+       },
+       {
+         name: 'My favorites 2',
+         songs: [
+           { name: 'song1', duration: 1342 },
+           { name: 'song2', duration: 3212 },
+           { name: 'song3', duration: 2123 }
+        ]
+       },
+       {
+         name: 'My favorites 3',
+         songs: [
+           { name: 'song1', duration: 1342 },
+           { name: 'song2', duration: 3212 },
+           { name: 'song3', duration: 2123 }
+        ]
+       },
+       {
+         name: 'My favorites 4',
+         songs: [
+           { name: 'song1', duration: 1342 },
+           { name: 'song2', duration: 3212 },
+           { name: 'song3', duration: 2123 }
+        ]
+       }
+     ]
+   }
+ }
 
-class Aggregate extends React.Component{
+class PlaylistCounter extends React.Component{
   render(){
     return (
-      <div className="aggregate" style={{...defaultStyle, width: '40%', display:'inline-block'}}>
-        <h2 style={{color: '#333'}}>Number Text</h2>
+      <div className="playlist-counter" style={{...defaultStyle, width: '40%', display:'inline-block'}}>
+        <h2 style={{ color: '#333' }}>{this.props.playlists.length} playlists</h2>
+      </div>
+    )
+  }
+}
+
+class HoursCounter extends React.Component{
+  render(){
+    const allSongs = this.props.playlists.reduce((songs, eachPlaylist)=>{
+      return songs.concat(eachPlaylist.songs) 
+    },[])
+    const totalDuration = allSongs.reduce((sum, eachSong)=>{
+      return sum + eachSong.duration
+    },0)
+    return (
+      <div className="hours-counter" style={{...defaultStyle, width: '40%', display:'inline-block'}}>
+        <h2 style={{ color: '#333' }}>{Math.floor(totalDuration/120)} Hours</h2>
       </div>
     )
   }
@@ -30,14 +85,15 @@ class Filter extends React.Component{
 
 class Playlist extends React.Component{
   render(){
+    const {name, songs} = this.props.playlist
     return(
       <div style={{...defaultStyle, display: "inline-block", width:"25%"}}>
         <img/>
-        <h3>Playlist name</h3>
+        <h3>Playlist {name}</h3>
         <ul>
-          <li>Song 1</li>
-          <li>Song 2</li>
-          <li>Song 3</li>
+          {songs.map(song =>
+            <li>{song.name}</li>
+          )}
         </ul>
       </div>
     )
@@ -45,20 +101,43 @@ class Playlist extends React.Component{
 }
 
 
-function App() {
-  const name = 'Abdulaziz'
-  return (
-    <div className="App" style={defaultStyle}>
-      <h1>Title</h1>
-      <Aggregate/>
-      <Aggregate/>
-      <Filter/>
-      <Playlist/>
-      <Playlist/>
-      <Playlist/>
-      <Playlist/>
-    </div>
-  );
+class App extends React.Component {
+  constructor(){
+    super();
+    this.state = {serverData: {}}
+  }
+
+  componentDidMount(){
+    setTimeout(() => {
+      this.setState({serverData : fakeServerData});
+    }, 1000)
+  }
+  render(){
+    return (
+      
+      <div className="App" style={defaultStyle}>
+        {this.state.serverData.user ?
+          <div>
+            <h1>
+              {this.state.serverData.user.name}'s Playlist
+            </h1>
+          
+            
+            <PlaylistCounter playlists={this.state.serverData.user.playlists}/>
+            <HoursCounter playlists={this.state.serverData.user.playlists}/>
+            
+
+            <Filter/>
+            {
+              this.state.serverData.user.playlists.map(playlist => 
+                <Playlist playlist = {playlist} />
+              )
+            }
+          </div> : <h2 style={defaultStyle}>Loading...</h2>
+        }
+      </div>
+    );
+  }
 }
 
 export default App;
